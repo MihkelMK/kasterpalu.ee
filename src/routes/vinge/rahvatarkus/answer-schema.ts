56487089e4b8fd7917e53bc80369b8062d2722a6
Mihkel Martin Kasterpalu <qpeuitovxg@use.startmail.com>
2025-02-18 13:33:07 +0200

    Rahvatarkus: increase max length of answer to 200 and question to 75

--- src/routes/vinge/rahvatarkus/answer-schema.ts
+++ src/routes/vinge/rahvatarkus/answer-schema.ts
@@ -5,7 +5,7 @@ export const formSchema = z.object({
 	answer: z
 		.string()
 		.min(2, 'Vastus peab olema vähemalt 2 tähemärki.')
-		.max(150, 'Vastus ei või olla pikem kui 150 tähemärki.'),
+		.max(200, 'Vastus ei või olla pikem kui 200 tähemärki.'),
 	altcha: z.string()
 });
 
--- src/routes/vinge/rahvatarkus/question-schema.ts
+++ src/routes/vinge/rahvatarkus/question-schema.ts
@@ -4,7 +4,7 @@ export const formSchema = z.object({
 	question: z
 		.string()
 		.min(2, 'Küsimus peab olema vähemalt 2 tähemärki.')
-		.max(50, 'Küsimus ei või olla pikem kui 50 tähemärki.'),
+		.max(75, 'Küsimus ei või olla pikem kui 75 tähemärki.'),
 	altcha: z.string()
 });
 
