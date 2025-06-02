0c328d3f66eaf98c1bed11488c6bc6aad2323e38
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2025-06-02 17:36:08 +0300

    Init paraglide

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,24 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { paraglideMiddleware } from '$lib/paraglide/server';
 import { SESH_SECRET } from '$env/static/private';
 import type { Handle } from '@sveltejs/kit';
 import { sveltekitSessionHandle } from 'svelte-kit-sessions';
-
-export const handle: Handle = sveltekitSessionHandle({
-	secret: SESH_SECRET
-});
 
 declare module 'svelte-kit-sessions' {
 	interface SessionData {
 		userId: string;
 	}
 }
+
+const originalHandle: Handle = sveltekitSessionHandle({ secret: SESH_SECRET });
+
+const handleParaglide: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request, locale }) => {
+		event.request = request;
+
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		});
+	});
+
+export const handle = sequence(originalHandle, handleParaglide);
